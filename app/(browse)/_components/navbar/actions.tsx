7de8530bc078ccb4cbd3c7c50c785cbf04b3d213
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-01-10 09:23:56 -0800

    search
    added functions to search so that clicking buttons causes event change.

---
+++ app/(browse)/_components/navbar/actions.tsx
@@ -0,0 +1,8 @@
+export const Actions = () => {
+    return (
+        <div>
+        actions
+    </div>
+    )
+}
+
--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -2,13 +2,13 @@
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { SearchIcon } from "lucide-react";
+import { SearchIcon, X } from "lucide-react";
 import {useRouter} from "next/navigation";
 import {useState} from "react";
 import qs from 'query-string'
 
 
-const Search = () => {
+export const Search = () => {
     const router = useRouter()
     const [value, setValue] = useState("")
 
@@ -25,16 +25,27 @@ const Search = () => {
         router.push(url)
     }
 
+    const onClear = () => {
+        setValue("")
+    }
+
   return (
       <form
-          onSubmit={() => {}}
+          onSubmit={onSubmit}
           className='relative w-full lg:w-[400px] flex items-center'
       >
           <Input
               placeholder="Search"
               className='rounded-r-none focus-visible:right-0 focus-visible:ring-transparent focus-visible:ring-offset-0'
-
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
           />
+          {value && (
+              <X
+                className='absolute top-2.5 right-14 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition'
+                onClick={onClear}
+              />
+          )}
           <Button
             type='submit'
             size='sm'
