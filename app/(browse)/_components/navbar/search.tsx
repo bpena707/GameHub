d28296845a7c7146f73623a82132a0f724a3ef42
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-01-10 22:03:09 -0800

    sidebar
    implemented collapsable sidebar with icons to expand end collapse along with tooltip indicators to show user what the button does

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -18,7 +18,7 @@ export const Search = () => {
         if (!value) return
 
         const url = qs.stringifyUrl({
-            url: "/",
+            url: "/search",
             query: { term: value },
         }, { skipEmptyString: true })
 
---
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -0,0 +1,24 @@
+// determines if sidebar is collapsed or expanded to full width
+'use client'
+
+import {useSidebar} from "@/store/useSidebar";
+import {state} from "sucrase/dist/types/parser/traverser/base";
+import {cn} from "@/lib/utils";
+
+interface WrapperProps {
+    children: React.ReactNode
+}
+
+export const Wrapper = ({
+    children,
+}: WrapperProps) => {
+    const { collapsed } = useSidebar((state)=> state)
+    return (
+        <aside className={cn(
+            'fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50',
+            collapsed && "w-[70px]"
+        )}>
+            {children}
+        </aside>
+    )
+}
